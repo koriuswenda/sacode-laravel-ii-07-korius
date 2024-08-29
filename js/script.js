cfc8dfc5325382616fb45a84ae34f67dca6e2306
korius kussa wenda <koriuswenda@gmail.com>
2024-08-29 17:57:11 +0700

    feat: tambah file index, js, css, dan images

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//  single line comment
+
+
+
+
+/*
+    multiline comment
+    bisa tulis juga disini 
+*/
+
+
+var namaDepan = "Korius";
+var namaBelakang = "Wenda";
+
+document
+.getElementById("nama")
+.innerHTML = namaDepan + namaBelakang;
